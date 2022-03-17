d4cba18fd9e8a25eea00c7bc20b3695c1360838f
Jade Cormier <cormier.jade@gmail.com>
2022-03-17 18:20:34 +0100

    add bookmark model - jonction table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_17_171829) do
+ActiveRecord::Schema.define(version: 2022_03_17_171943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2022_03_17_171829) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "address_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["address_id"], name: "index_bookmarks_on_address_id"
+    t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +43,6 @@ ActiveRecord::Schema.define(version: 2022_03_17_171829) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookmarks", "addresses"
+  add_foreign_key "bookmarks", "users"
 end
