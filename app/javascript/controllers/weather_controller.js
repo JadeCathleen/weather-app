5418a77ec5d0c71ad19bc6defa6a183a4fb5fe11
Jade Cormier <cormier.jade@gmail.com>
2022-03-20 23:29:20 +0100

    working weather but keys open and code to dry

---
+++ app/javascript/controllers/weather_controller.js
@@ -0,0 +1,51 @@
+import { Controller } from "@hotwired/stimulus"
+import { main } from "@popperjs/core";
+
+const baseMapboxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/';
+const mapboxKey = "pk.eyJ1IjoiamFkZWNhdGhsZWVuIiwiYSI6ImNrdmNlbTVuZzA1MHkydW4zMnNjbnkyeXYifQ.YRpgYnQ56ZJE8D2h9BSp0g"
+
+const baseWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?"
+const weatherKey = "332e2598be279e1cb6f27010cc70094c";
+
+// const getWeather = (data) => {
+//   const latitude = data["features"][0]["geometry"]["coordinates"][1];
+//   const longitude = data["features"][0]["geometry"]["coordinates"][0];
+//   fetch(`${baseWeatherUrl}lat=${latitude}&lon=${longitude}&appid=${weatherKey}&units=metric`)
+//   .then(response => response.json())
+//   .then((data2) => {
+//     this.cityTarget.innerHTML = `${data2["name"]}, ${data2["sys"]["country"]}`
+//     this.descriptionTarget.innerHTML = `${data2["weather"][0]["main"]} - ${data2["weather"][0]["description"]}`
+//     this.temperatureTarget.innerHTML = `${Math.round(data2["main"]["temp"])}°C`
+//   });
+// };
+
+export default class extends Controller {
+  static targets = ['form', 'city', 'description', 'temperature']
+
+  connect() {
+    console.log("Hello from our first Stimulus controller")
+  };
+
+
+  coordinates(event) {
+    // console.log("You clicked on Submit !")
+    event.preventDefault();
+    // this.cityTarget.innerHTML = this.formTarget.value;
+    fetch(`${baseMapboxUrl}${this.formTarget.value}.json?access_token=${mapboxKey}`)
+    .then(response => response.json())
+    .then((data) => {
+        // getWeather(data);
+          const latitude = data["features"][0]["geometry"]["coordinates"][1];
+          const longitude = data["features"][0]["geometry"]["coordinates"][0];
+          fetch(`${baseWeatherUrl}lat=${latitude}&lon=${longitude}&appid=${weatherKey}&units=metric`)
+          .then(response => response.json())
+          .then((data2) => {
+            this.cityTarget.innerHTML = `${data2["name"]}, ${data2["sys"]["country"]}`
+            this.descriptionTarget.innerHTML = `${data2["weather"][0]["main"]} - ${data2["weather"][0]["description"]}`
+            this.temperatureTarget.innerHTML = `${Math.round(data2["main"]["temp"])}°C`
+          });
+    });
+    this.formTarget.value = "";
+  }
+
+}
