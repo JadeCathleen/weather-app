33f0802478cc27e46429334ba7b95285a9b54c88
Jade Cormier <cormier.jade@gmail.com>
2022-03-21 11:07:31 +0100

    add api keys hidden working

--- app/javascript/controllers/weather_controller.js
+++ app/javascript/controllers/weather_controller.js
@@ -2,10 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 import { main } from "@popperjs/core";
 
 const baseMapboxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/';
-const mapboxKey = "1"
 
 const baseWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?"
-const weatherKey = "1";
 
 // const getWeather = (data) => {
 //   const latitude = data["features"][0]["geometry"]["coordinates"][1];
@@ -21,6 +19,7 @@ const weatherKey = "1";
 
 export default class extends Controller {
   static targets = ['form', 'city', 'description', 'temperature']
+  static values = { mapboxKey: String, openKey: String}
 
   connect() {
     console.log("Hello from our first Stimulus controller")
@@ -31,13 +30,13 @@ export default class extends Controller {
     // console.log("You clicked on Submit !")
     event.preventDefault();
     // this.cityTarget.innerHTML = this.formTarget.value;
-    fetch(`${baseMapboxUrl}${this.formTarget.value}.json?access_token=${mapboxKey}`)
+    fetch(`${baseMapboxUrl}${this.formTarget.value}.json?access_token=${this.mapboxKeyValue}`)
     .then(response => response.json())
     .then((data) => {
         // getWeather(data);
           const latitude = data["features"][0]["geometry"]["coordinates"][1];
           const longitude = data["features"][0]["geometry"]["coordinates"][0];
-          fetch(`${baseWeatherUrl}lat=${latitude}&lon=${longitude}&appid=${weatherKey}&units=metric`)
+          fetch(`${baseWeatherUrl}lat=${latitude}&lon=${longitude}&appid=${this.openKeyValue}&units=metric`)
           .then(response => response.json())
           .then((data2) => {
             this.cityTarget.innerHTML = `${data2["name"]}, ${data2["sys"]["country"]}`
