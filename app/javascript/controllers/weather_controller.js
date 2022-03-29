4ef28bd8ae09227f487431ff418b47c5ef3791de
Jade Cormier <cormier.jade@gmail.com>
2022-03-29 13:17:48 +0200

    remove unuseful comments in js weather controller

--- app/javascript/controllers/weather_controller.js
+++ app/javascript/controllers/weather_controller.js
@@ -5,18 +5,6 @@ const baseMapboxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/';
 
 const baseWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?"
 
-// const getWeather = (data) => {
-//   const latitude = data["features"][0]["geometry"]["coordinates"][1];
-//   const longitude = data["features"][0]["geometry"]["coordinates"][0];
-//   fetch(`${baseWeatherUrl}lat=${latitude}&lon=${longitude}&appid=${weatherKey}&units=metric`)
-//   .then(response => response.json())
-//   .then((data2) => {
-//     this.cityTarget.innerHTML = `${data2["name"]}, ${data2["sys"]["country"]}`
-//     this.descriptionTarget.innerHTML = `${data2["weather"][0]["main"]} - ${data2["weather"][0]["description"]}`
-//     this.temperatureTarget.innerHTML = `${Math.round(data2["main"]["temp"])}°C`
-//   });
-// };
-
 export default class extends Controller {
   static targets = ['form', 'city', 'description', 'temperature', 'icon', 'card', 'add', 'createForm', 'addButton']
   static values = { mapboxKey: String, openKey: String}
@@ -26,13 +14,10 @@ export default class extends Controller {
   };
 
   displayInformation(event) {
-    // console.log("You clicked on Submit !")
     event.preventDefault();
-    // this.cityTarget.innerHTML = this.formTarget.value;
     fetch(`${baseMapboxUrl}${this.cityTarget.innerHTML}.json?access_token=${this.mapboxKeyValue}`)
     .then(response => response.json())
     .then((data) => {
-        // getWeather(data);
           const latitude = data["features"][0]["geometry"]["coordinates"][1];
           const longitude = data["features"][0]["geometry"]["coordinates"][0];
           fetch(`${baseWeatherUrl}lat=${latitude}&lon=${longitude}&appid=${this.openKeyValue}&units=metric`)
